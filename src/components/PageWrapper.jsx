0c6779e1232e35a0964dca81978bd2f0becb0857
Reiduarig <giraudier82@gmail.com>
2025-10-17 13:04:34 +0200

    modificaciones diseño pagewrapper

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -1,6 +1,6 @@
 export function PageWrapper({ children }) {
     return (
-        <div className="min-h-dvh bg-gradient-to-b from-cyan-200 to-white to-[60vh]">
+        <div className="min-h-dvh bg-gradient-to-br from-blue-100 via-pink-100 to-violet-200 to-[70vh]">
             {children}
         </div>
     )
